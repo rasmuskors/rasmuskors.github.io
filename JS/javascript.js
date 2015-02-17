ecbba53c78d9b69660dce09f5cdb9f8737b2c2af
rasmuskors <rasmusk6rs@hotmail.com>
2015-02-17 10:41:16 +0800

    9th

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,8 +1,79 @@
 window.onload = function () {
-    document.getElementById('close').onclick = function () {
-        this.parentNode.parentNode.parentNode
-        .removeChild(this.parentNode.parentNode);
+    document.getElementById('close1').onclick = function () {
+        document.getElementById('work1').style.display='none';
+         document.getElementById('close1').style.display='none';
+        
+        document.getElementById('work2').style.display='block';
+         document.getElementById('close2').style.display='block';
+        document.getElementById('work3').style.display='block';
+         document.getElementById('close3').style.display='block';
+        return false;
+    };
+    document.getElementById('close2').onclick = function () {
+        document.getElementById('work2').style.display='none';
+         document.getElementById('close2').style.display='none';
+        
+                                    document.getElementById('work1').style.display='block';
+         document.getElementById('close1').style.display='block';
+        document.getElementById('work3').style.display='block';
+         document.getElementById('close3').style.display='block';
+        return false;
+    };
+    document.getElementById('close3').onclick = function () {
+        document.getElementById('work3').style.display='none';
+         document.getElementById('close3').style.display='none';
+        
+                document.getElementById('work1').style.display='block';
+         document.getElementById('close1').style.display='block';
+        document.getElementById('work2').style.display='block';
+         document.getElementById('close2').style.display='block';
         return false;
     };
 };
 
+
+
+
+
+
+function textFunction() {
+     document.getElementById('text1').style.display='block';
+     document.getElementById('text2').style.display='none';
+     document.getElementById('text3').style.display='none';
+    document.getElementById('text4').style.display='none';
+           YetAnotherFunction();
+}
+         
+         function textFunction2() {
+           document.getElementById('text2').style.display='block';
+         document.getElementById('text1').style.display='none';
+           document.getElementById('text3').style.display='none';
+           document.getElementById('text4').style.display='none';
+           YetAnotherFunction();
+         }
+         
+         
+         function textFunction3() {
+           document.getElementById('text3').style.display='block';
+         document.getElementById('text1').style.display='none';
+          document.getElementById('text2').style.display='none';
+           document.getElementById('text4').style.display='none';
+           YetAnotherFunction();
+         }
+         
+         function textFunction4() {
+           document.getElementById('text4').style.display='block';
+         document.getElementById('text1').style.display='none';
+          document.getElementById('text2').style.display='none';
+           document.getElementById('text3').style.display='none';
+           YetAnotherFunction();
+         }
+         
+         function textFunction5() {
+           document.getElementById('text4').style.display='none';
+         document.getElementById('text1').style.display='none';
+          document.getElementById('text2').style.display='none';
+           document.getElementById('text3').style.display='none';
+           YetAnotherFunction();
+         } 
+    
